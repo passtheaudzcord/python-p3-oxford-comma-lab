0aec223718fdfd78ca65a319eed214b4fe1e4bcd
Audrey <chipmanaudrey@gmail.com>
2024-08-12 20:03:30 -0700

    demo done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if isinstance(items, list) and len(items) == 1:
+        return items[0]
+    elif isinstance(items, list) and len(items) ==2:
+        new_list = " and ".join(items)
+        return new_list
+    elif isinstance(items, list) and len(items) >= 3:
+        last_item = items[-1]
+        remaining_items = ", ".join(items[:-1])
+        final_list = f"{remaining_items}, and {last_item}"
+        return final_list
